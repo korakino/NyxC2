9fa5b1041de2669febf366ed3fb497f247091581
Bastien FAUGIER <133578470+korakino@users.noreply.github.com>
2026-04-14 18:00:53 +0000

    Add print for debug

--- Server_main.py
+++ Server_main.py
@@ -4,13 +4,14 @@ def server():
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     soc.bind(("127.0.0.1", 2600))
     soc.listen(1)
-    x = 0
-    while x != 5:
+    print("Server is ready and listening")
+    while True:
+        print("waiting for connection")
         (clientsocket, addressguest) = soc.accept()
         print(f"Connection from {addressguest}")
         clientsocket.send(b"Hello there !")
         print(clientsocket.recv(16000))
         clientsocket.close()
-        x+=1
     soc.close()
     return
+server()
