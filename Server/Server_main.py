3e9b1cbc50b2a1f3575543a827f830e9a0dc722d
kora <133578470+korakino@users.noreply.github.com>
2026-07-04 16:17:10 +0200

    Update mc kill

--- Server/Server_main.py
+++ Server/Server_main.py
@@ -58,12 +58,15 @@ def server():
                             print("mc rename <old> <new> : Rename a target's alias/IP")
                             print("mc connect <target>   : Test connection to a specific target")
                             print("mc help               : Display this help menu")
-                            print("mc kill               : close script")
+                            print("mc kill  <target|*>   : close script for a target")
                             print("<target|*> <command>  : Send a command to target(s). * for all targets")
                             print("------------------------------------\n")
                     
                         case "kill":
-                            soc.close()
+                            target = order.split(" ")[1]
+                            
+                            
+                            send_message(target + "#", dict_ip)
                             return
                     
                     
